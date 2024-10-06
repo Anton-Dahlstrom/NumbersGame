42367a7564025b6a50f554bbcb87f083651aa3f2
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-10-06 23:40:58 +0200

    added comments

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // Generates a number between 1 and 20.
             Random rnd = new();
-            int randomNumber = rnd.Next(21);
+            int randomNumber = rnd.Next(21); 
+            
             int guess;
             int maxGuesses = 5;
             string finalMessage = $"Tyvärr du lyckades inte gissa rätt tal på {maxGuesses} försök.";
+            // Loop that asks the user to guess which number was generated. 
             for (int i = 0; i < maxGuesses; i++) 
             {
                 Console.Write("Skriv in din gissning: ");
@@ -17,16 +20,20 @@
                 {
                     Console.WriteLine("Skriv in ett heltal!");
                 }
+                // If the guess is correct the loop breaks.
                 if(CheckGuess(guess, randomNumber))
                 {
                 finalMessage = "Wooho! Du klarade det!";
                 break;
                 }
+                // Otherwise the user gets a hint.
                 string feedback = (guess < randomNumber) ? "Tyvärr du gissade för lågt!" : "Tyvärr du gissade för högt!";
                 Console.WriteLine(feedback);
-            };
+            }
             Console.WriteLine(finalMessage);
         }
+
+        // Kontrollerar ifall gissningen stämmer.
         static bool CheckGuess(int guess, int num)
         {
             if (guess == num){ return true; }
